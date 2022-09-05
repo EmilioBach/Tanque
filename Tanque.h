4138c746d6a7c9ac1ebd05d011f5000c61530eb3
EmilioBach <112587950+EmilioBach@users.noreply.github.com>
2022-09-05 19:36:57 -0300

    Update Tanque.h

--- Tanque.h
+++ Tanque.h
@@ -10,14 +10,20 @@
 
 class Tanque{
   public:
-    Tanque(int m1p, int m1n, int m2p, int m2n); //definimos pines para motores M = motor 1 = Motor 1 P o N = positivo o negativo
+    Tanque(int m1p, int m1n, int m2p, int m2n); //definimos constructor para motores M = motor 1 = Motor 1 P o N = positivo o negativo
+    void motorConfig(int m1p, int m1n, int m2p, int m2n);
     void adelante();
     void atras();
     void der();
     void izq();
     void detener();
    private:
-    int _m1p, _m1n, _m2p, _m2n;
-}; //no olvidar
+    //Motor 1
+    int _m1p;
+    int _m1n;
+    //Motor 2
+    int _m2p;
+    int _m2n;
+}; //no olvidar ;
 
 #endif
