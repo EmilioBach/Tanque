518432209ddc6e9fadb607dbbcdef75e2d330cfe
EmilioBach <112587950+EmilioBach@users.noreply.github.com>
2022-09-28 18:24:53 -0300

    Add files via upload

---
+++ Examples/Example1/Example_No_1.ino
@@ -0,0 +1,22 @@
+#include <Tanque.h>
+
+Tanque tank; //instancia a la clase
+
+void setup() {
+  tank.info(10,11,12,13); //indicar pines
+
+}
+
+void loop() {
+  tank.avanza(); //el tanque avanza
+  delay(2000);
+  tank.izquierda(); //el tanque gira a la izquierda
+  delay(1000);
+  tank.derecha(); //el tanque gira a la derecha
+  delay(1000);
+  tank.retrocede(); //el tanque avanza
+  delay(2000);
+  tank.detener(); //el tanque se detiene
+  delay(3000);
+
+}
